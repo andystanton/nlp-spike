b11ad7b3cff9182ce49ca336661760eae1b895f0
Andy Stanton <andystanton@gmail.com>
2014-07-28 10:40:44 +0100

    Updates

--- src/main/scala/NLPBastard.scala
+++ src/main/scala/NLPBastard.scala
@@ -1,26 +1,43 @@
-import opennlp.tools.doccat.{DocumentCategorizerME, DocumentSampleStream}
+import java.io._
+
+import opennlp.tools.doccat.{DoccatModel, DocumentCategorizerME, DocumentSampleStream}
 import opennlp.tools.util.PlainTextByLineStream
+
+case class Statement(sentence: String, sentiment: String)
 
 object NLPBastard {
 
-  def main(args: Array[String]): Unit = {
-    val is = Thread.currentThread().getContextClassLoader.getResourceAsStream("en-sentiment.train")
-    val lineStream = new PlainTextByLineStream(is, "UTF-8")
+  val testStatements = Seq(
+    Statement("I like turtles", "positive"),
+    Statement("I hate turtles", "negative"),
+    Statement("carpets are bad", "negative"),
+    Statement("I hate chairs", "negative"),
+    Statement("I don't mind bears", "neutral")
+  )
+
+  def learnTrainingFile = {
+    val inputStream = Thread.currentThread().getContextClassLoader.getResourceAsStream("en-sentiment.train")
+    val lineStream = new PlainTextByLineStream(inputStream, "UTF-8")
     val sampleStream = new DocumentSampleStream(lineStream)
-
     val model = DocumentCategorizerME.train("en", sampleStream)
+    inputStream.close()    
+    model
+  }
+  
+  def writeBinFile(model: DoccatModel) {
+    val binFile = new File("en-sentiment.bin")
+    if (binFile.exists()) binFile.delete()
 
-    is.close()
+    val fos: FileOutputStream = new FileOutputStream("en-sentiment.bin")
 
-    val d = new DocumentCategorizerME(model)
+    val modelOut = new BufferedOutputStream(fos)
+    model.serialize(modelOut)
+  }
 
-    val likeStatement = "I like turtles"
-    val hateStatement = "I hate turtles"
-
-    val likeOutcome = d.getBestCategory(d.categorize(likeStatement))
-    val hateOutcome = d.getBestCategory(d.categorize(hateStatement))
-
-    println(s"$likeStatement is $likeOutcome")
-    println(s"$hateStatement is $hateOutcome")
+  def loadModelFromBinFile = {
+    val inputStream: FileInputStream = new FileInputStream(new File("en-sentiment.bin"))
+    val model = new DoccatModel(inputStream)
+    inputStream.close()
+    model
   }
 }
---
+++ src/test/scala/NLPBastardSpec.scala
@@ -0,0 +1,38 @@
+import opennlp.tools.doccat.DocumentCategorizerME
+import org.scalatest.{Matchers, FunSpec}
+
+class NLPBastardSpec extends FunSpec with Matchers {
+  val testStatements = Seq(
+    Statement("I like turtles", "positive"),
+    Statement("I hate turtles", "negative"),
+    Statement("carpets are bad", "negative"),
+    Statement("I hate chairs", "negative"),
+    Statement("I don't mind bears", "neutral")
+  )
+
+  def checkTestStatements(implicit categorizer: DocumentCategorizerME) {
+    testStatements.foreach(testStatement => {
+      val sentiment = categorizer.getBestCategory(categorizer.categorize(testStatement.sentence))
+      println(s"Checking '${testStatement.sentence}'")
+      println(s" - Expected: '${testStatement.sentiment}' Actual: '$sentiment")
+      sentiment shouldBe testStatement.sentiment
+    })
+  }
+
+  describe("the NLP Bastard") {
+    it("can generate a model based on a training file") {
+      implicit val categorizer = new DocumentCategorizerME(NLPBastard.learnTrainingFile)
+
+      checkTestStatements
+    }
+
+    it("can load a model from a binary file") {
+      NLPBastard.writeBinFile(NLPBastard.learnTrainingFile)
+
+      val model = NLPBastard.loadModelFromBinFile
+      implicit val categorizer = new DocumentCategorizerME(model)
+
+      checkTestStatements
+    }
+  }
+}
